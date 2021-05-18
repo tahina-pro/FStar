55f2b51f4d3acdd19186ef987d18272e2da648fb
Tahina Ramananandro <taramana@microsoft.com>
2021-05-17 17:30:08 -0700

    complete missing snap

---
+++ src/ocaml-output/FStar_Version.ml
@@ -0,0 +1,7 @@
+open FStar_Util
+let dummy () = ();;
+FStar_Options._version := "0.9.7.0-alpha1";;
+FStar_Options._platform := "Linux_x86_64";;
+FStar_Options._compiler := "OCaml 4.12.0";;
+FStar_Options._date := "2021-05-15 06:24:58 +0000 ";;
+FStar_Options._commit:= "fd70c2e477d41a421028175302c7a838468c78d8 (dirty)";;
