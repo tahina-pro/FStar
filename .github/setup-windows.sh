05cfea863ed1529b73dd2a92737322f1ed5e77ba
Tahina Ramananandro <taramana@microsoft.com>
2023-04-08 16:37:32 -0700

    assume opam installs into /usr/local/bin

--- .github/setup-windows.sh
+++ .github/setup-windows.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 # Install Z3 and the opam package dependencies
 # We use the Everest script; this will automatically install OCaml.
 # We assume an everest checkout in the same directory as this script.
 # The GitHub Actions workflow should take care of cloning everest.
 cwd=$(cd $(dirname $0); pwd -P)
+cd $cwd
+echo "export PATH=/usr/local/bin:$PATH" >> $HOME/.bash_profile # opam installs there
+source env.sh
 cd $cwd/everest
-./everest --yes check || {
-    echo '. "$HOME/.opam/opam-init/init.sh" > /dev/null 2>/dev/null' >> $HOME/.bash_profile &&
-    source $HOME/.bash_profile &&
-    ./everest --yes check
-}
+./everest --yes check
