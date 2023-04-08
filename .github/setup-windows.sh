55e7cc9e3d0a162a680bc42a309503ccfbcb7082
Tahina Ramananandro <taramana@microsoft.com>
2023-04-08 16:22:01 -0700

    something wrong here. where is opam?

--- .github/setup-windows.sh
+++ .github/setup-windows.sh
@@ -7,5 +7,7 @@
 cwd=$(cd $(dirname $0); pwd -P)
 cd $cwd/everest
 ./everest --yes check || {
-    source $HOME/.bash_profile && ./everest --yes check
+    source $HOME/.bash_profile &&
+        echo "PATH=$PATH" &&
+        exit 1
 }
