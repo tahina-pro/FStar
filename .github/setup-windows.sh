1752790926b611c069fac47860eef23229c8f5ca
Tahina Ramananandro <taramana@microsoft.com>
2023-04-08 16:15:35 -0700

    we need 2 rounds of ./everest check

--- .github/setup-windows.sh
+++ .github/setup-windows.sh
@@ -6,4 +6,6 @@
 # The GitHub Actions workflow should take care of cloning everest.
 cwd=$(cd $(dirname $0); pwd -P)
 cd $cwd/everest
-./everest --yes check
+./everest --yes check || {
+    source $HOME/.bash_profile && ./everest --yes check
+}
